ab51dc81a45414c6dcb6a16e52dfe72ed47935b3
Paul Bowen <48794746+pb475@users.noreply.github.com>
2024-10-14 15:44:11 +0100

    tings

--- build_all.sh
+++ build_all.sh
@@ -10,3 +10,24 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 pyinstaller --onefile acctop.py
+
+# Check if the directory exists before creating it
+if [ ! -d "$HOME/bin/" ]; then
+    mkdir "$HOME/bin/"
+fi
+
+mv dist/acctop "$HOME/bin/acctop"
+
+# Add bin directory to PATH in shell configuration files (bashrc and zshrc for common ones)
+for shell_config in "$HOME/.bashrc" "$HOME/.zshrc"; do
+    if [ -f "$shell_config" ]; then
+        echo 'export PATH="$PATH:$HOME/bin"' >> "$shell_config"
+    fi
+done
+
+# Add bin directory to PATH in shell configuration files (bashrc and zshrc for common ones)
+for shell_config in "$HOME/.bashrc" "$HOME/.zshrc"; do
+    if [ -f "$shell_config" ]; then
+        source "$shell_config"
+    fi
+done
