c0e15516fe474fb87652c29519b8579cf5cf1dea
Paul Bowen <48794746+pb475@users.noreply.github.com>
2024-10-14 15:58:30 +0100

    update buildall

--- build_all.sh
+++ build_all.sh
@@ -15,26 +15,26 @@ pip install -r requirements.txt
 pyinstaller --onefile acctop.py
 
 # Check if the directory exists before creating it
-if [ ! -d "$HOME/bin/" ]; then
-    mkdir "$HOME/bin/"
+if [ ! -d "$HOME/.bin/" ]; then
+    mkdir "$HOME/.bin/"
 fi
 
 # Remove the previous version of acctop if it exists
-if [ -f "$HOME/bin/acctop" ]; then
-    rm "$HOME/bin/acctop"
+if [ -f "$HOME/.bin/acctop" ]; then
+    rm "$HOME/.bin/acctop"
 fi
 
-# Move the new version of acctop to the bin directory
-mv dist/acctop "$HOME/bin/acctop"
+# Move the new version of acctop to the .bin directory
+mv dist/acctop "$HOME/.bin/acctop"
 
-# Add bin directory to PATH in shell configuration files (bashrc and zshrc for common ones)
+# Add .bin directory to PATH in shell configuration files (bashrc and zshrc for common ones)
 for shell_config in "$HOME/.bashrc" "$HOME/.zshrc"; do
     if [ -f "$shell_config" ]; then
-        echo 'export PATH="$PATH:$HOME/bin"' >> "$shell_config"
+        echo 'export PATH="$PATH:$HOME/.bin"' >> "$shell_config"
     fi
 done
 
-# Add bin directory to PATH in shell configuration files (bashrc and zshrc for common ones)
+# Add .bin directory to PATH in shell configuration files (bashrc and zshrc for common ones)
 for shell_config in "$HOME/.bashrc" "$HOME/.zshrc"; do
     if [ -f "$shell_config" ]; then
         source "$shell_config"
