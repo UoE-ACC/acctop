ee8bf73c5bddcd91561138dc4a10301379e54adc
Paul Bowen <48794746+pb475@users.noreply.github.com>
2024-10-14 15:31:25 +0100

    update

--- build_all.sh
+++ build_all.sh
@@ -1,11 +1,10 @@
-#!~
-
+#!/bin/bash
 
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
 pip install --upgrade pip
 pip install --upgrade pip
-pip install requirements.txt
+pip install -r requirements.txt
 
 pyinstaller --onefile acctop.py
